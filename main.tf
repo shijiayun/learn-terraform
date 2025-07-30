24b8069590b68fa155cd8b1e222a57388764451f
shijiayun <shijyme@gmail.com>
2025-07-30 16:58:26 +0800

    s

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
-  tags         = ["web", "dev"]
+  tags         = ["foo"]
 
   boot_disk {
     initialize_params {
